f7e135ed1502344b4f5d66975f5b92a9b7be7b2a
eebann <ebanemmanuel@live.com>
2021-12-15 12:31:19 -0600

    added bootstrap icons also done with create post page

--- src/pages/write/Write.js
+++ src/pages/write/Write.js
@@ -1,15 +1,21 @@
 import React from 'react'
-
+import './write.css'
 function Write() {
 	return (
 		<div className='write'>
-			<form className='writeForm'>
-			<div className='writeFormGroup'>
-				<label htmlFor='title'>Title</label>
-			 <input type='file'    id="fileInput"/>
-			 <input type='text'   placeholder='text'/>
-
-		</div>
+			<img className='writeImg' src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="postImage"/>
+		<form className='writeForm'>
+				<div className='writeFormGroup'>
+						<label htmlFor='fileInput'>
+							<i className="writeIcon bi bi-plus-circle"> 	</i>
+						</label>
+					<input type='file'  id="fileInput" style={{display:"none"}}/>
+					<input type='text'   placeholder='Title' className="writeInput" autoFocus="true"   />
+				</div>
+				<div className='writeFormGroup'>
+					<textarea className="writeTextarea" type="text" placeholder='Write Your Story'></textarea>
+				</div>
+				 <button className='writeButton'> Submit</button>
 		</form>
 		</div>
 	)
